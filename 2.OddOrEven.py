d939742121be7584880c20b57082252ef0655e8a
ljuben-martinoski <ljuben.martinoski@outlook.de>
2025-12-07 18:20:25 +0100

    3 commit

---
+++ 2.OddOrEven.py
@@ -0,0 +1,13 @@
+"""
+Odd or Even: 
+Create a function to check if a number is odd or even"""
+
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is EVEN.")
+else:
+    print("The number is ODD.")
+    
+        
--- 4.AreaOfaCircle.py
+++ 4.AreaOfaCircle.py
@@ -6,7 +6,7 @@ Write a program that calculates the area of a circle given its radius.
 
 user = float(input("Enter the radius of your circkle and i will calculate the Area: "))
 
-area = (user * user) * 3.14
+area = (user * user) * 3.14 # assogning the formula for calculating the area of a circle
 
 print(area)
 
